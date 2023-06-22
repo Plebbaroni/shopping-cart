ea5b07b0045bf6754e9d7a599aacdaa87d5a5274
Jared <gorillacyborgz@gmail.com>
2023-06-22 20:20:04 +0800

    Added Navbar.

---
+++ src/components/navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/navbar.css"
+import { ShoppingCart } from "phosphor-react";
+
+export const Navbar = () => {
+    return(
+        <div className="navbar">
+            <Link to="/" className="logo">camelCase Records</Link>
+            <div className = "links">
+                <Link to="store" className="link">Products</Link>
+                <button
+                    className="cartbutton"
+                    //onClick={openCart}
+                    >
+                        <ShoppingCart size = {32}/>
+                    </button>
+            </div>
+        </div>
+    );
+}
+
